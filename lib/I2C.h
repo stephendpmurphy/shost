b57a670ddfb3796954a6275eda1fb23a6d9208a2
Noeël Moeskops <noeelmoeskops@Hotmail.com>
2022-01-23 20:16:35 +0100

    I²C and Protocol documentation

--- lib/I2C.h
+++ lib/I2C.h
@@ -8,16 +8,48 @@
 #include "shost.h"
 #include "Protocol.h"
 
-// TODO needs documentation
-
-class I2C: public Protocol {
+class I2C : public Protocol {
 public:
+    /**
+     * I²C Master
+     */
     I2C();
-private:
-    void _write(shost_xfer_t* xfer);
-    void _read(shost_xfer_t* xfer);
 
+private:
+    /**
+     * Write using I²C.
+     * @param xfer requires:
+     *  -   address,    7 bit address of slave.
+     *  -   _register,  register to read.
+     */
+    void _write(shost_xfer_t *xfer);
+
+    /**
+     * Read using I²C.
+     * @param xfer requires:
+     *  -   address,    7 bit address of slave.
+     *  -   _register,  register to write.
+     */
+    void _read(shost_xfer_t *xfer);
+
+    /**
+     * Write data to I²C device
+     * @param address 7bit address of salve
+     * @param reg register to write data to
+     * @param data pointer to byte array
+     * @param data_size size of data array
+     * @return 0 if success, 1 if no response and 2 if hardware failed.
+     */
     uint8_t i2c_write(uint8_t address, uint8_t reg, uint8_t *data, size_t data_size);
+
+    /**
+     * Read data from I2C device
+     * @param address address of slave
+     * @param reg register to read
+     * @param buffer store read data here
+     * @param buffer_size read size
+     * @return 0 if success, 1 if no response and 2 if hardware failed.
+     */
     uint8_t i2c_read(uint8_t address, uint8_t reg, uint8_t *buffer, size_t buffer_size);
 };
 
--- lib/Protocol.h
+++ lib/Protocol.h
@@ -7,20 +7,83 @@
 
 #include "shost.h"
 
+/**
+ * Protocol base class, used by I²C and SPI to create a abstraction for communication.
+ */
 class Protocol{
 public:
+    /**
+     * Name of the protocol
+     */
     const char* name;
 
+    /**
+     * Protocol object, should be created by child to give a proper name used for documentation
+     * @param name name of the protocol
+     */
     Protocol(const char* name);
 
+    /**
+     * read data using set protocol.
+     * Read data is returned in rx_buff
+     * @param xfer requires:
+     *  -   rx_len,     length of data to read
+     *  -   clk,        clock speed to use (Hertz)
+     *  -   channel,    MPSSE channel to use
+     *  -   rx_buff,    stores read data
+     *  -   xferType    set to READ
+     *
+     *  Please note that depending on the protocol being used additional parameters may be required.
+     *  See the relevant protocol (_read) for more information.
+     *
+     *  @throws std::system_error when things go wrong
+     *  The operation was successful if no exception was raised.
+     */
     void read(shost_xfer_t* xfer);
+
+    /**
+     * Write data using set protocol.
+     * @param xfer requires:
+     *  -   tx_len,     length of data to write
+     *  -   clk,        clock speed to use (Hertz)
+     *  -   channel,    MPSSE channel to use
+     *  -   tx_buff,    pointer to data array to write
+     *  -   xferType    set to WRITE
+     *
+     *  Please note that depending on the protocol being used additional parameters may be required.
+     *  See the relevant protocol (_write) for more information.
+     *
+     *  @throws std::system_error when things go wrong
+     *  The operation was successful if no exception was raised.
+     */
     void write(shost_xfer_t* xfer);
+
+    /**
+     * Not yet implemented!
+     * @param xfer
+     */
     void read_write(shost_xfer_t* xfer);
 
 protected:
+    /**
+     * MPSSE instance object used for interfacing with FT223x.
+     * Open and close the connection in every function.
+     * See https://github.com/stephendpmurphy/shost/issues/43
+     */
     struct mpsse_context* mpsse;
+
 private:
+
+    /**
+     * Protocol specific implementation of the write command.
+     * @param xfer supply documentation on required parameters!
+     */
     void virtual _write(shost_xfer_t* xfer) = 0;
+
+    /**
+     * Protocol specific implementation of the read command.
+     * @param xfer supply documentation on required parameters!
+     */
     void virtual _read(shost_xfer_t* xfer) = 0;
 };
 
